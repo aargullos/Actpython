e2880fb9bd88e639a2e367cdc4c7c0c327e4b86f
notarnold <95426496+aargullos@users.noreply.github.com>
2022-02-16 15:26:11 +0100

    Update main.py

--- Act5/main.py
+++ Act5/main.py
@@ -1,1 +1,11 @@
+import functions
 
+def main():
+    x = functions.valor()
+    i = functions.valor()
+    x, i = functions.change_val(x, i)
+    print(x, i)
+
+
+if __name__ == '__main__':
+    main()
